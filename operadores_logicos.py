d2a54c7ed59d11302b932d356f9d0c2398a99a39
DinaMalaver <santiagoavellafonseca@gmail.com>
2025-03-07 09:30:42 -0500

    NOT,AND

--- operadores_logicos.py
+++ operadores_logicos.py
@@ -18,4 +18,13 @@ op4 = not op2
 7. AND
 8. OR
 9. =
+NOTA: si hay operaciones en el mismo nivel de jerarquia, se resuelven de izquierda a derecha
 '''
+
+
+op1 = False
+op2 = True
+op3 = False
+op4 = True
+
+resultado = not op1 and op2 or op3 and not op4
