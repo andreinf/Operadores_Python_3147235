8775e23eb3a3038704495e602111ce263b6bcfe9
DinaMalaver <santiagoavellafonseca@gmail.com>
2025-03-07 08:59:05 -0500

    ope

---
+++ operadores_logicos.py
@@ -0,0 +1,21 @@
+
+
+op1 = False
+op2 = True
+op3 = op1 or op2
+print(op3)
+
+#operador not
+op4 = not op2
+
+'''JERARQUIA DEFINITIVA DE OPERADORES
+1. ()
+2. **
+3. +, -
+4. *, /, %
+5. >, <, !=, ==, >=, <=
+6. NOT
+7. AND
+8. OR
+9. =
+'''
